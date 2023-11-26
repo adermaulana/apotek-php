4051c8d321e0583bd7ab5a115744ea9ace5c2be7
Ade Rahmat Maulana <adermaulana15@gmail.com>
2023-11-26 21:37:14 +0800

    fix

--- admin/pembelian/tambah.php
+++ admin/pembelian/tambah.php
@@ -180,10 +180,10 @@ if(isset($_POST['simpan'])){
    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="col-lg-12">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Data Penjualan</h1>
+        <h1 class="h2">Data Pembelian</h1>
       </div>
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Tambah Penjualan</h1>
+        <h1 class="h2">Tambah Pembelian</h1>
       </div>
 
 <div class="col-lg-8">
